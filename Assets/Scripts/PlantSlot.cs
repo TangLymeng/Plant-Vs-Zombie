3dc4a27672008b96aa706341bcae260f7f5008f6
TangLymeng <tanglymeng2@gmail.com>
2024-02-26 11:43:37 +0700

    add ability to plant

---
+++ Assets/Scripts/PlantSlot.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlantSlot : MonoBehaviour
+{
+    public Sprite plantSprite;
+
+    public GameObject plantObject;
+
+    public int price;
+
+    public Image icon;
+
+    public TextMeshProUGUI priceText;
+
+    private GameManager gms;
+    
+    private void Start() {
+        gms = GameObject.Find("GameManager").GetComponent<GameManager>();
+        GetComponent<Button>().onClick.AddListener(BuyPlant);
+    }
+
+    private void BuyPlant() {
+        gms.BuyPlant(plantObject, plantSprite);
+    }
+
+    public void OnValidate()
+    {
+        if (plantSprite) {
+            icon.enabled = true;
+            icon.sprite = plantSprite;
+            priceText.text = price.ToString();
+        } else {
+            icon.enabled = false;
+        }
+    }
+
+}
